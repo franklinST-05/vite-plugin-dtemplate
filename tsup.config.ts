d244415b71b0c561884e9bf0aaa93d337ad13481
frank <franklinteixeira205@gmail.com>
2024-02-21 21:46:36 -0300

    feat: module js

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,10 +2,9 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
     entryPoints: ["src/index.ts"],
-    format: "cjs",
+    format: ["cjs", "esm"],
     splitting: false,
     clean: true,
     dts: true,
     external: ["vite"],
-    minify: true,
 });
